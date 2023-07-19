fc6c6d1c89d4cee3406132d718b1d29fabe943c1
Mike Peters <info@mikepeters.io>
2023-07-18 23:08:41 -0400

    Add Behance projects

--- login.php
+++ login.php
@@ -19,6 +19,321 @@
 
 		<div class="row">
     		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/70315789/Omega-Wallet" target="_blank">
+						<div class="thumbnail">
+							<img src="img/omega@2x.png" alt="Omega Wallet">
+						</div>
+						<div class="details">
+							<p>iOS App</p>
+							<h3>Omega Wallet</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/61515197/SendGrid-Redesign" target="_blank">
+						<div class="thumbnail">
+							<img src="img/sendgrid@2x.png" alt="SendGrid">
+						</div>
+						<div class="details">
+							<p>Website</p>
+							<h3>SendGrid</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/56956963/Aura-Financial" target="_blank">
+						<div class="thumbnail">
+							<img src="img/aura@2x.png" alt="Aura">
+						</div>
+						<div class="details">
+							<p>Website</p>
+							<h3>Aura Financial</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53341313/Gulf-Coast-Honda-Dealers" target="_blank">
+						<div class="thumbnail">
+							<img src="img/honda@2x.png" alt="Gulf Coast Honda">
+						</div>
+						<div class="details">
+							<p>Website</p>
+							<h3>Gulf Coast Honda</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53342471/Helix-Marketing-Platform" target="_blank">
+						<div class="thumbnail">
+							<img src="img/helix@2x.png" alt="Helix">
+						</div>
+						<div class="details">
+							<p>iOS App</p>
+							<h3>Helix Platform</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53342389/2017-Audi-Q3-Crossover" target="_blank">
+						<div class="thumbnail">
+							<img src="img/audi@2x.png" alt="Audi Q3">
+						</div>
+						<div class="details">
+							<p>Email</p>
+							<h3>Audi Q3</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/55013269/2018-Toyota-Camry" target="_blank">
+						<div class="thumbnail">
+							<img src="img/toyota@2x.png" alt="Toyota Camry">
+						</div>
+						<div class="details">
+							<p>Email</p>
+							<h3>Toyota Camry</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53342201/Mercedes-Benz-Winter-Event" target="_blank">
+						<div class="thumbnail">
+							<img src="img/mercedes@2x.png" alt="Mercedez-Benz Winter Event">
+						</div>
+						<div class="details">
+							<p>E-mail</p>
+							<h3>Mercedez-Benz Winter Event</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53340897/Trade-In-Trade-Up" target="_blank">
+						<div class="thumbnail">
+							<img src="img/purl@2x.png" alt="Trade In, Trade Up">
+						</div>
+						<div class="details">
+							<p>Website</p>
+							<h3>Trade In, Trade Up</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/66973439/Music-Midtown-2015" target="_blank">
+						<div class="thumbnail">
+							<img src="img/mm@2x.png" alt="Music Midtown">
+						</div>
+						<div class="details">
+							<p>Website, iOS App</p>
+							<h3>Music Midtown</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53342113/Cone-Middour-Partners" target="_blank">
+						<div class="thumbnail">
+							<img src="img/cone@2x.png" alt="Cone Middour">
+						</div>
+						<div class="details">
+							<p>Website</p>
+							<h3>Cone Middour</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/67192745/Breeders-Cup-Experiences" target="_blank">
+						<div class="thumbnail">
+							<img src="img/breeders@2x.png" alt="Breeders Cup">
+						</div>
+						<div class="details">
+							<p>Print</p>
+							<h3>Breeders Cup</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/67101669/Kemp-Tennis-Academy" target="_blank">
+						<div class="thumbnail">
+							<img src="img/kemp@2x.png" alt="Kemp Tennis Academy">
+						</div>
+						<div class="details">
+							<p>Branding</p>
+							<h3>Kemp Tennis Academy</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/67025963/Atlanta-Skyline" target="_blank">
+						<div class="thumbnail">
+							<img src="img/atl@2x.png" alt="Atlanta Skyline">
+						</div>
+						<div class="details">
+							<p>Illustration</p>
+							<h3>Atlanta Skyline</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53519769/Cupids-Prey" target="_blank">
+						<div class="thumbnail">
+							<img src="img/cupid@2x.png" alt="Cupid's Prey">
+						</div>
+						<div class="details">
+							<p>Print</p>
+							<h3>Cupid's Prey</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53519297/Tabernacle-VIP" target="_blank">
+						<div class="thumbnail">
+							<img src="img/tabernacle@2x.png" alt="The Tabernacle">
+						</div>
+						<div class="details">
+							<p>Print</p>
+							<h3>The Tabernacle</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53519935/Hazard-County-Money-Jam" target="_blank">
+						<div class="thumbnail">
+							<img src="img/moneyjam@2x.png" alt="Hazard County">
+						</div>
+						<div class="details">
+							<p>Print</p>
+							<h3>Hazard County</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+    		<div class="col-xs-12
+                col-sm-6
+                col-md-6
+                col-lg-6">
+				<div class="project">
+					<a href="https://www.behance.net/gallery/53518965/Quark-(QRK)" target="_blank">
+						<div class="thumbnail">
+							<img src="img/quark@2x.png" alt="Quark Coin">
+						</div>
+						<div class="details">
+							<p>Branding</p>
+							<h3>Quark Coin</h3>
+						</div>
+					</a>
+				</div>
+    		</div>
+		</div>
+
+		<div class="row">
+    		<div class="col-xs-12
                 col-sm-12
                 col-md-12
                 col-lg-12">
