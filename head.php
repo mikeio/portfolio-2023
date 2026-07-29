9617c70f6699cda1c3e84902a06b8fc1502d9a0c
Mike Peters <mapdesigns@gmail.com>
2026-07-29 15:29:07 -0400

    added new alt tags

--- head.php
+++ head.php
@@ -1,30 +1,39 @@
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Mike Peters | Online Portfolio</title>
-    <meta name="description" content="Hello! I’m Mike. I help creative studios design digital interactive experiences.">
+    <title>Mike Peters | Senior Digital & Interactive Designer</title>
+    <meta name="description" content="Hello, I'm Mike Peters, a Senior Designer specializing in digital interactive experiences. Explore my portfolio of award-winning web, UI/UX, and app designs.">
+    <meta name="author" content="Mike Peters">
 
-    <!-- Google tag (gtag.js) -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://mikepeters.io/">
+    <meta property="og:title" content="Mike Peters | Senior Digital & Interactive Designer">
+    <meta property="og:description" content="Hello, I'm Mike Peters, a Senior Designer specializing in digital interactive experiences. Explore my portfolio of award-winning web, UI/UX, and app designs.">
+    <meta property="og:image" content="https://mikepeters.io/img/og-image.jpg">
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://mikepeters.io/">
+    <meta property="twitter:title" content="Mike Peters | Senior Digital & Interactive Designer">
+    <meta property="twitter:description" content="Hello, I'm Mike Peters, a Senior Designer specializing in digital interactive experiences. Explore my portfolio of award-winning web, UI/UX, and app designs.">
+    <meta property="twitter:image" content="https://mikepeters.io/img/og-image.png">
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-VHJT44DFJT"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-    
       gtag('config', 'G-VHJT44DFJT');
     </script>
 
-    <script src="https://kit.fontawesome.com/518b7edfd0.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
-
+    <script src="https://kit.fontawesome.com/518b7edfd0.js" crossorigin="anonymous" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
-    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
-
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous" defer></script>
+    <script src="js/scripts.js" defer></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" as="font">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" as="style">
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
